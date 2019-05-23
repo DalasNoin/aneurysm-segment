873315aa792452d91e3ef4429b05fc7e7df33195
lerm <sim.ler68@gmail.com>
2019-05-23 18:00:09 +0200

    patching moved forward

--- data/patching.py
+++ data/patching.py
@@ -3,26 +3,37 @@ import numpy as np
 from scipy import ndimage
 from matplotlib import pyplot as plt
 
-class PatchImage:
-    def __init__(self,name, tensor, shape=(40,40,40),stride=(35,35,35)):
-        self.patch_indices = self.patch_3d(tensor,shape,stride)
-        self.name = name
 
-    def patch_3d(self, tensor, shape, strides):
+class PatchImage:
+    def __init__(self, name, tensor, shape=(40, 40, 40), stride=(35, 35, 35)):
+        self.tensor = tensor
+        self.shape = shape
+        self.stride = stride
+        self.name = name
+        self.patch_indices = self.patch_3d()
+
+    def patch_3d(self):
         windows = [int(np.ceil((x - (x_patch // 2)) / x_stride)) for x, x_patch, x_stride in
-                   zip(tensor.shape, shape, strides)]
+                   zip(self.tensor.shape, self.shape, self.stride)]
         return windows
 
-    def get_patch(self, indices, tensor, shape, strides):
-        xyz_begin = [min(x_shape, x * stride) for x, stride, x_shape in zip(indices, strides, tensor.shape)]
-        xyz_end = [min(x_shape, x + x_patch) for x, x_patch, x_shape in zip(xyz_begin, shape, tensor.shape)]
+    def get_patch(self, indices):
+        xyz_begin = [min(x_shape, x * stride) for x, stride, x_shape in zip(indices, self.stride, self.tensor.shape)]
+        xyz_end = [min(x_shape, x + x_patch) for x, x_patch, x_shape in zip(xyz_begin, self.shape, self.tensor.shape)]
         xyz = xyz_begin + xyz_end
         return xyz
 
-    def cut_patch(self, tensor, patch):
-        return tensor[patch[0]:patch[3], patch[1]:patch[4], patch[2]:patch[5]]
+    def cut_patch(self, patch):
+        return self.tensor[patch[0]:patch[3], patch[1]:patch[4], patch[2]:patch[5]]
 
 
+
+    def iterate(self):
+        X, Y, Z = np.meshgrid(*[np.arange(value) for value in self.patch_indices])
+        X, Y, Z = X.flatten(), Y.flatten(), Z.flatten()
+        for indices in zip(X, Y, Z):
+            patch = self.get_patch(indices)
+            yield self.cut_patch(patch), indices, patch
 
 
 class Patch:
--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,9 @@ import pydicom
 from os import listdir
 from os.path import isfile, join
 import config
+from data import patching
+import pandas as pd
+from tqdm import tqdm
 
 
 def discover(path):
@@ -21,20 +24,23 @@ def extract_pixel_arrays(path_list):
 
 
 def extract_pixel_array(path):
+    if path[-3:] == "npy":
+        return np.load(path)
     image = pydicom.read_file(path)
     return image.pixel_array
 
 
 def preprocess_x_data(X):
     X = [arr.reshape(*arr.shape, 1) for arr in X]
-    
+
+
 def safe_mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
 
 class AneurysmData:
-    def __init__(self, path, size=(16, 16, 16), mock=False):
+    def __init__(self, path, size=None, mock=False):
         if not mock:
             path_image = os.path.join(path, "image")
             path_mask = os.path.join(path, "mask")
@@ -43,13 +49,13 @@ class AneurysmData:
             if size is not None:
                 self.images = self.downsampling(self.paths_image, size=size)
                 self.masks = self.downsampling(self.paths_mask, size=size)
-                self.images = self.normalize(self.images)
-                self.masks = self.to_binary(self.masks).astype("int")
-            #else:
-                #self.images = self.load(self.paths_image)
-                #self.masks = self.load(self.paths_mask)
-                #self.images = self.normalize(self.images)
-                #self.masks = self.to_binary(self.masks)
+                # self.images = self.normalize(self.images)
+                # self.masks = self.to_binary(self.masks).astype("int")
+            # else:
+            # self.images = self.load(self.paths_image)
+            # self.masks = self.load(self.paths_mask)
+            # self.images = self.normalize(self.images)
+            # self.masks = self.to_binary(self.masks)
 
         else:
             self.images = [np.random.randint(low=-100, high=100, size=(1, 110, 256 + i, 256 - i)) for i in range(100)]
@@ -62,30 +68,65 @@ class AneurysmData:
         zoom = [target / source for target, source in zip(size, image.shape)]
         image = ndimage.zoom(image, zoom=zoom)
         return image
-    
-    def load(self, paths):
+
+    def load_data(self, count=100):
+        self.images = self.load(self.paths_image, count=count)
+        self.masks = self.load(self.paths_mask, count=count)
+
+    def load(self, paths, count=100):
         images = list()
-        for path in paths:
+        for path in paths[:count]:
             image = extract_pixel_array(path)
             images.append(image)
         return images
 
     def element_wise_save(self):
         safe_mkdir(config.full_data_path)
-        mask_dir = join(config.full_data_path,"mask")
-        image_dir = join(config.full_data_path,"image")
+        mask_dir = join(config.full_data_path, "mask")
+        image_dir = join(config.full_data_path, "image")
         safe_mkdir(mask_dir)
         safe_mkdir(image_dir)
-        for image_path,image_name, mask_path, mask_name in zip(self.paths_image,self.image_names, self.paths_mask, self.mask_names):
-            
+        for image_path, image_name, mask_path, mask_name in zip(self.paths_image, self.image_names, self.paths_mask,
+                                                                self.mask_names):
             image = extract_pixel_array(image_path)
             image = self.normalize(image).astype("float32")
             self.save_single(image_dir, image_name, image)
             mask = extract_pixel_array(mask_path)
             mask = self.to_binary(mask)
             self.save_single(mask_dir, mask_name, mask)
-        
-    
+
+    def save_patches_ew(self):
+        safe_mkdir(config.full_data_path)
+        mask_dir = join(config.patch_data_path, "mask")
+        image_dir = join(config.patch_data_path, "image")
+        safe_mkdir(mask_dir)
+        safe_mkdir(image_dir)
+        print("Create Patches for the images")
+        self.run_patch(self.paths_image, self.image_names, image_dir)
+        print("Create patches for the masks")
+        self.records = pd.DataFrame(self.run_patch(self.paths_mask, self.mask_names, mask_dir, mask=True), columns = ["Indices", "patches", "filepath", "name", "positiv"])
+
+    def generate_filepath(self, target_dir, name, indices):
+        if "." in name:
+            name = name.split(".")[0]
+        filepath = join(target_dir, "{}-{}x{}x{}".format(name, *indices))
+        return filepath
+
+    def run_patch(self, paths, names, target_dir, mask=False):
+        if mask:
+            records = []
+        for path, name in tqdm(zip(paths, names), total=len(paths)):
+            image = extract_pixel_array(path)
+            pi = patching.PatchImage(name=name, tensor=image)
+            for sub_tensor, indices, patch in pi.iterate():
+                filepath = self.generate_filepath(target_dir, name, indices)
+                if mask:
+                    positive_eg = len(np.unique(sub_tensor)) !=1
+                    records.append([indices, patch, filepath, name, positive_eg])
+                np.save(filepath, sub_tensor)
+        if mask:
+            return records
+
     def downsampling(self, paths, size=(16, 16, 16)):
         images = list()
         for path in paths:
@@ -99,12 +140,12 @@ class AneurysmData:
         :param data:
         :return:
         """
-        if isinstance(data,list):
-            return [(element - np.mean(element))/np.std(element) for element in data]
+        if isinstance(data, list):
+            return [(element - np.mean(element)) / np.std(element) for element in data]
         return (data - np.mean(data)) / np.std(data)
 
     def to_binary(self, data):
-        if isinstance(data,list):
+        if isinstance(data, list):
             return [(element != 0).astype("int") for element in data]
         return data != 0
 
@@ -124,12 +165,12 @@ class AneurysmData:
                 ax[i, j].imshow(im, alpha=0.2, cmap="Greys_r")
                 ax[i, j].imshow(m == 0, alpha=0.8, cmap="cividis", vmin=0, vmax=1)
         return fig
-    
-    def save_single(self,path, name, tensor):
+
+    def save_single(self, path, name, tensor):
         new_path = join(path, name)
         np.save(new_path, tensor)
 
-    def save(self,path, names, tensors):
+    def save(self, path, names, tensors):
         new_paths = [join(path, name) for name in names]
         for tensor, path in zip(tensors, new_paths):
             np.save(path, tensor)
@@ -138,7 +179,11 @@ class AneurysmData:
         safe_mkdir(path)
         self.save(path, self.image_names, self.images)
         self.save(path, self.mask_names, self.masks)
-        
-if __name__=="__main__":
-    ad = AneurysmData(config.PATH, size = None)
-    ad.element_wise_save()
+
+
+if __name__ == "__main__":
+    # ad = AneurysmData(config.PATH, size=None)
+    # ad.element_wise_save()
+    ad = AneurysmData(config.full_data_path)
+    ad.save_patches_ew()
+    ad.records.to_csv(join(config.patch_data_path, "records.csv"))
